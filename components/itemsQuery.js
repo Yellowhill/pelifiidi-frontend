dfd719502a355427ab7a30bc22a54e8ef46c79bf
Jere Keltamaki <jere.keltamaki@enersoft.fi>
2019-05-12 10:54:24 +0300

    initial setup for locaStorage saving

--- components/itemsQuery.js
+++ components/itemsQuery.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import gql from 'graphql-tag';
-import { Query } from 'react-apollo';
+import { Query, ApolloConsumer } from 'react-apollo';
 import Items from './Items';
 import { ITEM_FRAGMENT } from '../graphql/fragments';
-
+import updateLocalStorage from '../lib/updateLocalStorage';
 const ITEMS_QUERY = gql`
 	query ITEMS_QUERY($first: Int!, $skip: Int!) {
-		itemsConnection(orderBy: publishDate_DESC, first: $first, skip: $skip) {
+		itemsConnection(orderBy: publishDate_DESC, first: $first, skip: $skip)
+			@connection(key: "item") {
 			edges {
 				node {
 					...ItemListInfo
@@ -25,24 +26,29 @@ const ITEMS_QUERY = gql`
 
 function ItemsQuery() {
 	return (
-		<Query query={ITEMS_QUERY} variables={{ first: 5, skip: 0 }}>
-			{({ data, loading, error, subscribeToMore, fetchMore }) => {
-				if (error) return <p>Error: {error}</p>;
-				if (loading) return <p>loading...</p>;
-				// console.log('itemsQuery.js - data: ', data);
-				const items = data.itemsConnection.edges;
-				const hasMoreItems = data.itemsConnection.pageInfo.hasNextPage;
+		<ApolloConsumer>
+			{(client) => (
+				<Query query={ITEMS_QUERY} variables={{ first: 5, skip: 0 }}>
+					{({ data, loading, error, subscribeToMore, fetchMore }) => {
+						if (error) return <p>Error: {error}</p>;
+						if (loading) return <p>loading...</p>;
+						// console.log('itemsQuery.js - data: ', data);
+						updateLocalStorage(client);
+						const items = data.itemsConnection.edges;
+						const hasMoreItems = data.itemsConnection.pageInfo.hasNextPage;
 
-				return (
-					<Items
-						items={items}
-						subscribeToMore={subscribeToMore}
-						fetchMore={fetchMore}
-						hasMoreItems={hasMoreItems}
-					/>
-				);
-			}}
-		</Query>
+						return (
+							<Items
+								items={items}
+								subscribeToMore={subscribeToMore}
+								fetchMore={fetchMore}
+								hasMoreItems={hasMoreItems}
+							/>
+						);
+					}}
+				</Query>
+			)}
+		</ApolloConsumer>
 	);
 }
 
